0bc8f20b6f84eda5d4fe0c96c18d2b18fd29ad5e
abbb1399 <abbb123@naver.com>
2021-06-06 14:53:57 +0900

    logout

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -7,17 +7,15 @@ const auth = async (req,res,next)=>{
     const decoded = jwt.verify(token, 'thisismynewcourse')
     const user = await User.findOne({_id : decoded._id, 'tokens.token' : token})
 
-    console.log(token)
-    console.log(decoded)
-    console.log(user)
-
     if(!user){
       throw new Error()
     }
+
+    req.token = token
     req.user = user
     next()
   }catch(e){
-    res.status(401).send({error: 'Please athenticate'})
+    res.status(401).send({error: 'Please authenticate'})
   }
 }
 
