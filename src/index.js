d272c60893d27d81f5bdf39eb88a2b60b2f6e4dc
abbb1399 <abbb123@naver.com>
2021-06-13 16:38:10 +0900

    file upload

--- src/index.js
+++ src/index.js
@@ -6,42 +6,10 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// 미들웨어
-// app.use((req,res,next)=>{
-//   console.log(req.method, req.path)
-//  if(req.method === 'GET'){
-//   res.send('Get requests are disabled')
-//  }else{
-//    next()
-//  }
-// })
-
-// app.use((req,res,next)=>{
-//   res.status(503).send('Site is currenlty down. Check back soon!')
-// })
-
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
 app.listen(port,()=>{
   console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// const main = async ()=>{
-//   // const task = await Task.findById('60bcc3a32b908f3420677042')
-//   // await task.populate('owner').execPopulate()
-//   // console.log(task.owner)
-
-//   const user = await User.findById('60bcc39d2b908f3420677040')
-//   await user.populate('tasks').execPopulate()
-//   console.log(user.tasks)
-// }
-
-// main()
